d296c400c62c6a3ce9505a99c254fe436c8830a7
nizilk <ailier22@126.com>
2024-10-16 21:54:03 +0800

    Menu

--- TablePat_Win/MainWindow.xaml.cs
+++ TablePat_Win/MainWindow.xaml.cs
@@ -25,16 +25,18 @@ namespace TablePat_Win
     /// </summary>
     public partial class MainWindow : Window
     {
-        private System.Windows.Forms.Screen screen = System.Windows.Forms.Screen.PrimaryScreen;
-        private NotificationManager notificationManager = new NotificationManager();
+        private System.Windows.Forms.Screen screen = System.Windows.Forms.Screen.PrimaryScreen;     // 屏幕 暂未用到
+        private NotificationManager notificationManager = new NotificationManager();    // 通知
         private DispatcherTimer timer = new DispatcherTimer();
         private DispatcherTimer timerMove = new DispatcherTimer();
 
+        // 全部动画资源的路径 -- 只用一次的
         public Uri[] ResourceOnce = {
             new Uri("pack://application:,,,/Resources/headpicture.gif"),       // 0 headpicture
             new Uri("pack://application:,,,/Resources/profilepicture.gif"),    // 1 profilepicture
             new Uri("pack://application:,,,/Resources/start.gif"),             // 2 start
         };
+        //  全部动画资源的路径 -- 随机切换的
         public Uri[] Resource = {
             new Uri("pack://application:,,,/Resources/movel.gif"),             // 0 movel
             new Uri("pack://application:,,,/Resources/mover.gif"),             // 1 mover
@@ -48,6 +50,7 @@ namespace TablePat_Win
             new Uri("pack://application:,,,/Resources/sleepl.gif"),            // 9 sleepl
         };
 
+
         public MainWindow()
         {
             InitializeComponent();
@@ -59,15 +62,20 @@ namespace TablePat_Win
             timerMove.Start();
         }
 
-        private int threshold = 7;
-        private void timer_Tick(object sender, EventArgs e)     // 1s时钟: 随机改变动画
+
+        // 1s时钟tick: 随机改变动画
+        private int threshold = 7;  // 初始阈值
+        private void timer_Tick(object sender, EventArgs e)     
         {
+            if (timer == null)  return;
+            if (!changeable)    return;
             if (Mouse.LeftButton == MouseButtonState.Pressed) return;
             if (Mouse.RightButton == MouseButtonState.Pressed) return;
-            int ranTrigger = new Random().Next(0, 50);
-            int ranType = new Random().Next(0, 44);
-            int ranAdd = new Random().Next(0, 2);
-            if (ranTrigger < threshold)
+
+            int ranTrigger = new Random().Next(0, 50);  // 是否更换动画
+            int ranType = new Random().Next(0, 54);     // 更换动画编号, move和relax可能性更大些
+            int ranAdd = new Random().Next(0, 2);       // 阈值每次有1/2几率+1(使得换动画概率增大)
+            if (ranTrigger < threshold)     // 动态缩放阈值, 控制更换动画的频率
             {
                 AnimationBehavior.SetSourceUri(pet, Resource[ranType % 10]);
                 threshold = threshold > 0 ? threshold - 5 : 0;
@@ -79,27 +87,33 @@ namespace TablePat_Win
             }
         }
 
-        private void timerMove_Tick(object sender,  EventArgs e)    // 1ms时钟: 移动窗口
+
+        // 1ms时钟tick: 移动窗口
+        private double step = 0.275;
+        private void timerMove_Tick(object sender,  EventArgs e)    
         {
             Uri state = AnimationBehavior.GetSourceUri(pet);
             if (state == null) return;
+            if (!moveable) return;
 
             int width = screen.Bounds.Width;    // 获取屏幕的宽度     
             int height = screen.Bounds.Height;  // 获取屏幕的高度
-            Point ptLeftUp = this.PointToScreen(new Point(0, 0));
-            Point ptRightDown = this.PointToScreen(new Point(this.ActualWidth, this.ActualHeight));
+            Point ptLeftUp = this.PointToScreen(new Point(0, 0));   // 左上角屏幕坐标
+            Point ptRightDown = this.PointToScreen(new Point(this.ActualWidth, this.ActualHeight));     // 右下角屏幕坐标
 
             if (state == Resource[0])
             {
-                if (ptLeftUp.X > -225)  this.Left -= 0.275;
+                if (ptLeftUp.X > -225)  this.Left -= step;
             }
             if (state == Resource[1])
             {
-                if (width - ptRightDown.X > -225) mainWin.Left += 0.275;
+                if (width - ptRightDown.X > -225) mainWin.Left += step;
             }
         }
 
-        private void mainWin_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)    // 鼠标拖动
+
+        // 鼠标拖动
+        private void mainWin_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)    
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
@@ -108,14 +122,18 @@ namespace TablePat_Win
             }
         }
 
-        private void mainWin_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)     // 松开鼠标, 播放动画"start.gif"
+
+        // 松开鼠标左键, 播放动画"start.gif"
+        private void mainWin_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)     
         {
             AnimationBehavior.AddAnimationCompletedHandler(pet, animationCompleted);
             AnimationBehavior.SetRepeatBehavior(pet, new RepeatBehavior(1));
             AnimationBehavior.SetSourceUri(pet, ResourceOnce[2]);
         }
 
-        public void animationCompleted(object sender, AnimationCompletedEventArgs e)    // 动画播放完毕时, 回到初始状态"relax.gif"
+
+        // 动画播放完毕时, 回到初始状态"relax.gif"
+        public void animationCompleted(object sender, AnimationCompletedEventArgs e)    
         {
 
             AnimationBehavior.SetSourceUri(pet, Resource[2]);
@@ -123,7 +141,9 @@ namespace TablePat_Win
             AnimationBehavior.RemoveAnimationCompletedHandler(pet, animationCompleted);
         }
 
-        private void pet_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)  // 单击触发随机对话, 通过通知显示, 后续需改进
+
+        // 单击触发随机对话, 通过通知显示, 后续需改进
+        private void pet_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)  
         {
             Random random = new Random();
             int randomNumber = random.Next(1, 7);
@@ -160,5 +180,93 @@ namespace TablePat_Win
             });
         }
 
+
+        /*********************** 右键选单 ***********************/
+        // 置于顶层
+        private void pinTop_Click(object sender, RoutedEventArgs e)
+        {
+            pinTop.IsChecked = !pinTop.IsChecked;
+            this.Topmost = pinTop.IsChecked;
+        }
+
+
+        // 调整大小 -- 小
+        private void small_Click(object sender, RoutedEventArgs e)
+        {
+            small.IsChecked = true;
+            mid.IsChecked = false;
+            large.IsChecked = false;
+            pet.Width = 200;
+            pet.Height = 200;
+            this.Height = 200;
+            step = 0.25;
+        }
+
+        // 调整大小 -- 中
+        private void mid_Click(object sender, RoutedEventArgs e)
+        {
+            small.IsChecked = false;
+            mid.IsChecked = true;
+            large.IsChecked = false;
+            pet.Width = 250;
+            pet.Height = 250;
+            this.Height = 250;
+            step = 0.275;
+        }
+
+        // 调整大小 -- 大
+        private void large_Click(object sender, RoutedEventArgs e)
+        {
+            small.IsChecked = false;
+            mid.IsChecked = false;
+            large.IsChecked = true;
+            pet.Width = 300;
+            pet.Height = 300;
+            this.Height = 300;
+            step = 0.3;
+        }
+
+
+        // 禁止走动
+        private bool moveable = true;
+        private void stopMove_Click(object sender, RoutedEventArgs e)
+        {
+            stopMove.IsChecked = !stopMove.IsChecked;
+            moveable = !stopMove.IsChecked;
+        }
+
+
+        // 坐下
+        private void sit_Click(object sender, RoutedEventArgs e) => AnimationBehavior.SetSourceUri(pet, Resource[6]);
+        
+
+        // 睡觉
+        private bool changeable = true;
+        private void sleep_Click(object sender, RoutedEventArgs e)
+        {
+            if (sleep.Header.ToString() == "睡觉")
+            {
+                changeable = false;
+                AnimationBehavior.SetSourceUri(pet, Resource[8]);
+                sleep.Header = "唤醒";
+                sit.IsEnabled = false;
+            }
+            else
+            {
+                changeable = true;
+                AnimationBehavior.SetSourceUri(pet, Resource[2]);
+                sleep.Header = "睡觉";
+                sit.IsEnabled = true;
+            }
+        }
+        
+        
+        // 退出
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        /*********************** 右键选单 ***********************/
     }
 }
