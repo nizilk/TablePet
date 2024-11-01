98ef9b8bd5c2d39f8fb0e571a8914bf877616689
PR0PHECY <brokenmoon233@gmail.com>
2024-11-01 23:23:34 +0800

    add custom messagebox

---
+++ TablePet.Win/ComicMessageBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace TablePet.Win.Messagebox
+{
+    public partial class ComicMessageBox : UserControl
+    {
+         private DispatcherTimer timer;
+
+        public ComicMessageBox()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3); 
+            timer.Tick += Timer_Tick;
+        }
+        
+        public void ShowMessage(string message)
+        {
+            MessageText.Text = message;
+            this.Visibility = Visibility.Visible;
+            timer.Start(); 
+        }
+        
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+            timer.Stop(); 
+        }
+    }
+}
