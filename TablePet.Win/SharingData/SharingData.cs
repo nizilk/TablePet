cad0d69cc2442d80ff811a76a89c9af25fdc4b48
PR0PHECY <brokenmoon233@gmail.com>
2024-11-04 12:16:29 +0800

    add 心情值共享

---
+++ TablePet.Win/SharingData/SharingData.cs
@@ -0,0 +1,13 @@
+﻿namespace TablePet.Win.SharingDatan
+{
+    public static class SharingData
+    {
+        public static int Favorability { get; set; } = 80;
+        
+        public static bool IsFavorabilityLow()
+        {
+            return SharingData.Favorability < 30;
+        }
+    }
+    
+}
