f99d473a144949c614cceaa2fa53b1a17caaea1d
nizilk <ailier22@126.com>
2024-11-12 18:43:22 +0800

    little fix on "notes" 注释了Calendar的数据库连接，不然测试不了

--- TablePet.Services/Controllers/CalendarService.cs
+++ TablePet.Services/Controllers/CalendarService.cs
@@ -43,7 +43,7 @@ namespace TablePet.Services.Controllers
 
             // 获取指定日期的事件，按时间排序
             string query = "SELECT id, start_time, description FROM CalendarEvents WHERE DATE(start_time) = @Date ORDER BY start_time";
-
+            /*
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
                 conn.Open();
@@ -65,7 +65,7 @@ namespace TablePet.Services.Controllers
                         }
                     }
                 }
-            }
+            }*/
 
             return events;
         }
